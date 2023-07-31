ab9d0fdc0be2d70c486d8abd83f10478b83d84a5
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-07-30 19:55:50 -0500

    Agregue archivo index.js (gustavo)

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function laCajaDePandora(numero){
     // proximamente escribiremos codigo aqui
-    }
+    if (typeof numero !== 'number' || !Number.isInteger(numero)) {
+        throw new Error('El parámetro debe ser un número entero.');
+      }
+    
+      if (numero % 2 === 0) {
+        // Es un número par, convertir a binario
+        return num.toString(2);
+      } else {
+        // Es un número impar, convertir a hexadecimal
+        return num.toString(16);
+      }
+}
