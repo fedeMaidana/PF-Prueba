80791e39389a759a335a7dced4a85fd4ba1de05f
Federico Maidana <fede_maidana4@hotmail.com>
2023-07-30 21:59:52 -0300

    Funcion con mis datos

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ function laCajaDePandora( numero ){
     if( numero % 2 === 0 ) return numero.toString( 2 )
     else return numero.toString( 16 )
 }
+
+function federico(){
+    const obj = {
+        nombre: 'Federico',
+        edad: 26,
+        nacionalidad: 'Argentino'
+    }
+
+    return obj
+}
